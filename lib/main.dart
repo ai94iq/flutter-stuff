2904489a0c5fab9391223b6592321654af1946ed
Abdulwahab Isam <abdoi94.iq@gmail.com>
2023-04-23 18:05:58 +0300

    basic 'Hello World'
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(body: GradientContainer()),
+  ));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 103, 58, 183),
+            Color.fromARGB(255, 118, 12, 111),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
